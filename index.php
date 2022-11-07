a7b1c97d89776aa76229784dbfb9fe3bd7e7a2cc
achchaimae <achchaimae1@gmail.com>
2022-11-07 17:03:52 +0100

    add some container stuff

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    Document</title>
+</head>
+<body>
+    <main>
+    <section class="navbar">
+    
+        <ul>
+            <li><a href="#">Home</a></li>
+            
+            <li><a href="#">About</a></li>
+            <li><a href="#">Courses</a></li>
+            <li><img src="assets/image/logo.png" alt="logo" class="logo"></li>
+            <li><a href="#">Events</a></li>
+            <li> <a href="#">Contact Us</a></li>
+            <li><img src="assets/image/Search_icon.png" alt="logo"></li>
+        </ul>
+    </section>
+    <section class="content">
+        <div class="silde_home">
+            <h3>“Success is no accident. It is hard work, perseverance, learning, studying,<br> sacrifice and most of all, love of what you are doing or learning to do.”
+
+                <br><br> ― Pelé, Brazillian pro footballer</h3>
+        </div>
+        <h1>News</h1><br><br><br>
+      <div class="news">
+            
+                <div class="container" >   
+                    <div>
+                        <img src="assets/image/illustartion/news.png" alt="new 1" class="img-news">
+                        <h3>Lorem ipsum</h3>
+                    </div>
+                    <div>
+                        <img src="assets/image/illustartion/news1.png" alt="new 2" class="img-news">
+                        <h3>Lorem ipsum</h3>
+                    </div>
+                    <div>
+                        <img src="assets/image/illustartion/news.png" alt="new 3" class="img-news">
+                        <h3>Lorem ipsum</h3>
+                    </div>
+                    <div>
+                        <img src="assets/image/illustartion/news4.png" alt="new 4" class="img-news">
+                        <h3>Lorem ipsum</h3>
+                    </div>
+                </div>
+        </div>
+    </section>
+    <section class="footer">
+    <footer>
+            <div class="social-media">
+                <a href="#" class="footer-logo"><img src="assets/image/icon/fb.png" alt="facebook logo"></a>
+                <a href="#"class="footer-logo"><img src="assets/image/icon/ig.png" alt="instagram logo"></a>
+                <a href="#"class="footer-logo"><img src="assets/image/icon/in.png" alt="linkedin logo"></a>
+                <a href="#"class="footer-logo"><img src="assets/image/icon/pinterest.png" alt="pinterst logo"></a>
+                <a href="#"class="footer-logo"><img src="assets/image/icon/be.png" alt="behance logo"></a>
+            
+            </div> 
+                <p class="copyright">
+                    copyright. designed and developed by <a href="https://github.com/Achchaimae">Achchaimae Khalaf</a>
+                </p>
+    </footer>
+    </section>
+    </main>
+</body>
+</html>

8bd50ec71deac25264f9236e24c6969903d5b46a
achchaimae <achchaimae1@gmail.com>
2022-12-18 01:26:22 +0100

    add pages

---
+++ assets/php/Courses.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/Courses.css">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+    <section class="navbar">
+    <ul>
+            <li><a href="../../index.php">Home</a></li>
+            
+            <li><a href="about.php">About</a></li>
+            <li><img src="../image/logo.png" alt="logo" class="logo"></li>
+            <li><a href="#">Courses</a></li>
+            <!-- <li><a href="#">Events</a></li> -->
+            <li> <a href="ContactUs.php">Contact Us</a></li>
+            <li><img src="../image/Search_icon.png" alt="logo"></li>
+    </ul>
+    </section>
+    <section class="categories">
+        <div class="branch">
+            <img src="../image/illustartion/math.png" alt="online learning" class="online-learning">
+            <h3>course 1</h3>
+        </div>
+        <div class="branch">
+            <img src="../image/illustartion/info.png" alt="online learning" class="online-learning">
+            <h3>course 2</h3>
+        </div>
+        <div class="branch">
+            <img src="../image/illustartion/eco.png" alt="online learning" class="online-learning">
+            <h3>course 3</h3>
+        </div>
+        <div class="branch">
+            <img src="../image/illustartion/geo.png" alt="online learning" class="online-learning">
+            <h3>course 4</h3>
+        </div>
+        <div class="branch">
+            <img src="../image/illustartion/physique.png" alt="online learning" class="online-learning">
+            <h3>course 5</h3>
+        </div>
+        <div class="branch">
+            <img src="../image/illustartion/law.png" alt="online learning" class="online-learning">
+            <h3>course 6</h3>
+        </div>
+        
+        
+    </section>
+    <section class="download">
+        <div class="pdfFile">
+            <p>test</p>
+            <button>download</button>
+        </div>
+        <div class="pdfFile">
+            <p>test</p>
+            <button>download</button>
+        </div>
+        <div class="pdfFile">
+            <p>test</p>
+            <button>download</button>
+        </div>
+        <div class="pdfFile">
+            <p>test</p>
+            <button>download</button>
+        </div>
+    </section>
+    <section class="footer">
+         <footer>
+                <div class="social-media">
+                                        <a href="#" class="footer-logo"><img src="../image/icon/fb.png" alt="facebook logo"></a>
+                                        <a href="#"class="footer-logo"><img src="../image/icon/ig.png" alt="instagram logo"></a>
+                                        <a href="#"class="footer-logo"><img src="../image/icon/in.png" alt="linkedin logo"></a>
+                                        <a href="#"class="footer-logo"><img src="../image/icon/pinterest.png" alt="pinterst logo"></a>
+                                        <a href="#"class="footer-logo"><img src="../image/icon/be.png" alt="behance logo"></a>
+                                    
+                </div> 
+                <p class="copyright">
+                    copyright. designed and developed by <a href="https://github.com/Achchaimae">Achchaimae Khalaf</a>
+                </p>
+         </footer>
+    </section> 
+    </main>
+</body>
+</html>

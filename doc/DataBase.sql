ad274292d1600efd9613f5bd860d19262fde9b79
achchaimae <achchaimae1@gmail.com>
2022-12-21 17:39:47 +0100

    db 🌚

---
+++ doc/DataBase.sql
@@ -0,0 +1,12 @@
+create database University;
+use University;
+create table Course(
+    CourseID int primary key,
+    CourseName varchar(50),
+    CourseTeacher varchar(50),
+    CourseTime varchar(50),
+    CoursePlace varchar(50),
+    CourseFile varchar(50)
+);
+
+

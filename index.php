76d886b41eaedc24a321ce16eddf9250f8ea9382
achchaimae <achchaimae1@gmail.com>
2022-12-21 17:38:36 +0100

    some update ❄

--- index.php
+++ index.php
@@ -1,132 +1,115 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/style.css">
-   </title>
+    </title>
 </head>
+
 <body>
     <main>
-    <section class="navbar">
-        <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="assets/php/about.php">About</a></li>
-            <li><img src="assets/image/logo.png" alt="logo" class="logo"></li>
-<<<<<<< HEAD
-            <li><a href="assets/php/Courses.php">Courses</a></li>
-            <li> <a href="assets/php/ContactUs.php">Contact Us</a></li>
-=======
-            <li><a href="#">Courses</a></li>
-            <li> <a href="assets/php/contact.php">Contact Us</a></li>
->>>>>>> ff9638b1585683aa58ad65fd1a08bdcbd30f85dc
-             <li><img src="assets/image/Search_icon.png" alt="logo"></li>
-        </ul> 
-    </section>
-    <section>
-        <div class="silde_home">
-            <h3>“Success is no accident. It is hard work, perseverance, learning, studying,<br> sacrifice and most of all, love of what you are doing or learning to do.”
+        <section class="navbar">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="assets/php/about.php">About</a></li>
+                <li><img src="assets/image/logo.png" alt="logo" class="logo"></li>
 
-                <br><br> ― Pelé, Brazillian pro footballer</h3>
-        </div>
-    </section>
-    <center><h1>News</h1></center><br><br><br>
-    <section class="galerie">
-        
-       
-        <div class="activite">
-                    <img src="assets/image/illustartion/news.png" alt="art">
-                    <div class="intro">
-                        <h3>Title</h3> 
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis neque soluta excepturi nihil quia hic asperiores veritatis libero ducimus eos optio doloribus recusandae</p>
-                    </div>
-                   
-                </div>
-                <div class="activite">
-                    <img src="assets/image/illustartion/news1.png" alt="art">
-                    <div class="intro">
-                        <h3>Title</h3> 
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis neque soluta excepturi nihil quia hic asperiores veritatis libero ducimus eos optio doloribus recusandae</p>
-                    </div>
-                   
-                </div>
-                <div class="activite">
-                    <img src="assets/image/illustartion/news2.png" alt="art">
-                    <div class="intro">
-                        <h3>Title</h3> 
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis neque soluta excepturi nihil quia hic asperiores veritatis libero ducimus eos optio doloribus recusandae</p>
-                    </div>
-                   
-                </div>
-                <div class="activite">
-                    <img src="assets/image/illustartion/news4.png" alt="art">
-                    <div class="intro">
-                        <h3>Title</h3> 
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis neque soluta excepturi nihil quia hic asperiores veritatis libero ducimus eos optio doloribus recusandae</p>
-                    </div>
-                   
-                </div>
-        <!-- <div class="news">
-            <div class="news_container">
-                <div class="new" id="new1">
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid, nihil iusto. Aperiam nobis suscipit numquam. Necessitatibus repellat, iure veritatis quis, inventore laudantium tempora vel magni quas, obcaecati recusandae saepe. Unde?</p>
-                </div>
-                <h2>lorem ipsum</h2>
+                <li><a href="assets/php/Courses.php">Courses</a></li>
+                <li> <a href="assets/php/ContactUs.php">Contact Us</a></li>
+                <li><img src="assets/image/Search_icon.png" alt="logo"></li>
+            </ul>
+        </section>
+        <section>
+            <div class="silde_home">
+                <h3>“Success is no accident. It is hard work, perseverance, learning, studying,<br> sacrifice and most of all, love of what you are doing or learning to do.”
+
+                    <br><br> ― Pelé, Brazillian pro footballer
+                </h3>
             </div>
-            <div class="news_container">
-                <div class="new"  id="new2">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus eum, dolores sunt eligendi dignissimos inventore esse incidunt vitae nostrum odit adipisci nesciunt fugiat dolorum quam cumque, omnis, libero nisi saepe!</p>
+        </section>
+        <center>
+            <h1>News</h1>
+        </center><br><br><br>
+        <section class="galerie">
+
+
+            <div class="activite">
+                <img src="assets/image/illustartion/news.png" alt="art">
+                <div class="intro">
+                    <h3>news</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis neque soluta excepturi nihil quia hic asperiores veritatis libero ducimus eos optio doloribus recusandae</p>
                 </div>
-                <h2>lorem ipsum</h2>
+
             </div>
-            <div class="news_container">
-                <div class="new"  id="new3">
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque quam accusamus natus, debitis neque dolorem molestiae in facilis rerum molestias praesentium voluptas voluptatum amet error sapiente fugit illo sequi tempora.</p>
+            <div class="activite">
+                <img src="assets/image/illustartion/news1.png" alt="art">
+                <div class="intro">
+                    <h3>news</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis neque soluta excepturi nihil quia hic asperiores veritatis libero ducimus eos optio doloribus recusandae</p>
                 </div>
-                <h2>lorem ipsum</h2>
+
             </div>
-            <div class="news_container">
-                <div class="new"  id="new4">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur labore, officiis dolores minus possimus distinctio aliquid quam veritatis asperiores placeat neque earum aut rem molestiae accusamus repudiandae autem quibusdam rerum.</p>
+            <div class="activite">
+                <img src="assets/image/illustartion/news2.png" alt="art">
+                <div class="intro">
+                    <h3>news</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis neque soluta excepturi nihil quia hic asperiores veritatis libero ducimus eos optio doloribus recusandae</p>
                 </div>
-                <h2>lorem ipsum</h2>
+
             </div>
-        </div>  
-              
-        <div>
-            <h1>Events</h1>
-        </div>
-        <div class="Events">
-                    <div class="Events_title"><h2> Coming Soon</h2></div>
-                    <div class="coming_soon">
-                        <img src="assets/image/illustartion/news.png" alt="new 1">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.</p>
-                    </div>
-                    <div class="Events_title"><h2> Last_events</h2></div>
-                    <div class="last_events">
-                        <img src="assets/image/illustartion/news.png" alt="new 1">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.</p>
-                    </div>
-                    
-                
-        </div> -->
-    </section>
-    <section class="footer">
-    <footer>
-         <div class="social-media">
-                <a href="#" class="footer-logo"><img src="assets/image/icon/fb.png" alt="facebook logo"></a>
-                <a href="#"class="footer-logo"><img src="assets/image/icon/ig.png" alt="instagram logo"></a>
-                <a href="#"class="footer-logo"><img src="assets/image/icon/in.png" alt="linkedin logo"></a>
-                <a href="#"class="footer-logo"><img src="assets/image/icon/pinterest.png" alt="pinterst logo"></a>
-                <a href="#"class="footer-logo"><img src="assets/image/icon/be.png" alt="behance logo"></a>
-            
-            </div> 
+            <div class="activite">
+                <img src="assets/image/illustartion/news4.png" alt="art">
+                <div class="intro">
+                    <h3>news</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis neque soluta excepturi nihil quia hic asperiores veritatis libero ducimus eos optio doloribus recusandae</p>
+                </div>
+
+            </div>
+           
+            </section>
+        </section>
+        <section>
+            <div>
+                <div class="Events_title">
+                    <h2> Coming Soon</h2>
+                </div>
+                <div class="coming_soon">
+                    <img src="assets/image/illustartion/news.png" alt="new 1">
+                    <h1>coming soon</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.</p>
+                </div>
+                <div class="Events_title">
+                    <h2> Last_events</h2>
+                </div>
+                <div class="last_events">
+                    <img src="assets/image/illustartion/news.png" alt="new 1">
+                    <h1>last events</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos laboriosam alias adipisci accusamus natus esse omnis, voluptas, id incidunt, nobis amet quis vel temporibus doloribus suscipit perspiciatis optio totam nemo.</p>
+                </div>
+
+
+            </div>
+
+        </section>
+        <section class="footer">
+            <footer>
+                <div class="social-media">
+                    <a href="#" class="footer-logo"><img src="assets/image/icon/fb.png" alt="facebook logo"></a>
+                    <a href="#" class="footer-logo"><img src="assets/image/icon/ig.png" alt="instagram logo"></a>
+                    <a href="#" class="footer-logo"><img src="assets/image/icon/in.png" alt="linkedin logo"></a>
+                    <a href="#" class="footer-logo"><img src="assets/image/icon/pinterest.png" alt="pinterst logo"></a>
+                    <a href="#" class="footer-logo"><img src="assets/image/icon/be.png" alt="behance logo"></a>
+
+                </div>
                 <p class="copyright">
                     copyright. designed and developed by <a href="https://github.com/Achchaimae">Achchaimae Khalaf</a>
                 </p>
-                </footer>
-    </section>
+            </footer>
+        </section>
     </main>
 </body>
+
 </html>

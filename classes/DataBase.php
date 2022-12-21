c4686dd22c1473708a448d607acebd60681b9839
achchaimae <achchaimae1@gmail.com>
2022-12-21 19:44:00 +0100

    database link 👌

---
+++ classes/DataBase.php
@@ -0,0 +1,17 @@
+<?php
+
+class Dbh
+{
+    private $host = "localhost";
+    private $user = "root";
+    private $pwd = "";
+    private $dbName = "University";
+
+    protected function connect()
+    {
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbName;
+        $pdo = new PDO($dsn, $this->user, $this->pwd);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    }
+}

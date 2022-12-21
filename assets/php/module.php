397dcdd6a5eadca871d44a46268d83536be311ac
achchaimae <achchaimae1@gmail.com>
2022-12-21 17:39:32 +0100

    add a page ✌

---
+++ assets/php/module.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/Courses.css">
+    <title>Courses</title>
+</head>
+
+<body>
+    <main>
+        <section class="navbar">
+            <ul>
+                <li><a href="../../index.php">Home</a></li>
+
+                <li><a href="about.php">About</a></li>
+                <li><img src="../image/logo.png" alt="logo" class="logo"></li>
+                <li><a href="#">Courses</a></li>
+                <!-- <li><a href="#">Events</a></li> -->
+                <li> <a href="ContactUs.php">Contact Us</a></li>
+                <li><img src="../image/Search_icon.png" alt="logo"></li>
+            </ul>
+        </section>
+        <section>
+            <div class="course-title">
+                <div class="course-img">
+                    <img src="../image/illustartion/math.png" alt="math">
+                </div>
+                <div class="course-info">
+                    <h1>mathematique</h1>
+                </div>
+            </div>
+
+        </section>
+        <section class="download">
+                <div class="pdfFile">
+                    <p>cour 1</p>
+                    <button>download</button>
+                </div>
+                <div class="pdfFile">
+                    <p>cour 2</p>
+                    <button>download</button>
+                </div>
+                <div class="pdfFile">
+                    <p>cour 3</p>
+                    <button>download</button>
+                </div>
+                <div class="pdfFile">
+                    <p>cour 4</p>
+                    <button>download</button>
+                </div>
+        </section>
+        <section class="footer space">
+            <?php include 'footer.php'; ?>
+        </section>
+    </main>
+</body>
+
+</html>
